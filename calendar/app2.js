bbb486739771cf95408de81fd2edc9f4ed42a205
selvachitra <133102807+selvachitra@users.noreply.github.com>
2023-09-26 11:53:50 +0530

    Add files via upload

---
+++ calendar/app2.js
@@ -0,0 +1,19 @@
+const monthE1 =document.getElementById("month");
+const dayNameE1 =document.getElementById("day");
+const dayNumE1 =document.getElementById("day-number");
+const yearE1 =document.getElementById("year");
+ 
+const date = new Date();
+
+const month = date.getMonth();
+
+monthE1.innerHTML =date.toLocaleDateString("en",{
+    month: "long" ,
+});
+
+dayNameE1.innerHTML =date.toLocaleDateString("en",{
+    weekday:"long",
+});
+
+dayNumE1.innerHTML =date.getDate();
+yearE1.innerHTML =date.getFullYear();
